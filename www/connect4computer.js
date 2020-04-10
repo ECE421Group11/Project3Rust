2d9491ccff76431fb1ef7bfc331e022e73b56586
Tymoore Jamal <tymoore@ualberta.ca>
2020-04-10 14:39:57 -0600

    added difficulty

--- www/connect4computer.js
+++ www/connect4computer.js
@@ -6,16 +6,35 @@ var name1 = ""
 
 
 hide();
+range_changed();
 document.getElementById("startbutton").addEventListener("click", startGame);
+document.getElementById("drange").addEventListener("change", range_changed);
+
+function range_changed(){
+    var diff = parseInt(document.getElementById("drange").value,10);
+    if (diff == 1){
+        document.getElementById("difficulty").innerHTML = "Easy"
+    }
+    if (diff == 2){
+        document.getElementById("difficulty").innerHTML = "Normal"
+    }
+    if (diff == 3){
+        document.getElementById("difficulty").innerHTML = "Hard"
+    }
+}
 
 function hide(){
     document.getElementById("names").style.display = "none";
     document.getElementById("gameboard").style.display = "none";
+    document.getElementById("changediff").style.display = "none";
+
 }
 
 function show(){
     document.getElementById("names").style.display = "block";
     document.getElementById("gameboard").style.display = "block";
+    document.getElementById("drange").style.display = "none";
+    document.getElementById("changediff").style.display = "block";
 }
 
 function setText(){
