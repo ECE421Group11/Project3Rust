18915dc25ff85baf53d73e26d7e7ac29d2c2ae3a
Glenn <47393384+glnnrhodes@users.noreply.github.com>
2020-04-14 16:38:44 -0600

    check_state moved to rust
    check_state moved to rust

--- pkg/project3_rust.js
+++ pkg/project3_rust.js
@@ -88,29 +88,6 @@ cachedTextDecoder.decode();
 function getStringFromWasm0(ptr, len) {
     return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
 }
-
-let heap_next = heap.length;
-
-function addHeapObject(obj) {
-    if (heap_next === heap.length) heap.push(heap.length + 1);
-    const idx = heap_next;
-    heap_next = heap[idx];
-
-    heap[idx] = obj;
-    return idx;
-}
-
-function dropObject(idx) {
-    if (idx < 36) return;
-    heap[idx] = heap_next;
-    heap_next = idx;
-}
-
-function takeObject(idx) {
-    const ret = getObject(idx);
-    dropObject(idx);
-    return ret;
-}
 /**
 */
 export function greet() {
@@ -154,97 +131,6 @@ export function check_state(js_ob) {
     }
 }
 
-/**
-* @returns {string}
-*/
-export function return_name() {
-    try {
-        wasm.return_name(8);
-        var r0 = getInt32Memory0()[8 / 4 + 0];
-        var r1 = getInt32Memory0()[8 / 4 + 1];
-        return getStringFromWasm0(r0, r1);
-    } finally {
-        wasm.__wbindgen_free(r0, r1);
-    }
-}
-
-/**
-* @param {string} game_number
-* @param {string} game_type
-* @param {string} player1_name
-* @param {string} player2_name
-* @param {string} winner_name
-*/
-export function insert_game(game_number, game_type, player1_name, player2_name, winner_name) {
-    var ptr0 = passStringToWasm0(game_number, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
-    var len0 = WASM_VECTOR_LEN;
-    var ptr1 = passStringToWasm0(game_type, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
-    var len1 = WASM_VECTOR_LEN;
-    var ptr2 = passStringToWasm0(player1_name, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
-    var len2 = WASM_VECTOR_LEN;
-    var ptr3 = passStringToWasm0(player2_name, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
-    var len3 = WASM_VECTOR_LEN;
-    var ptr4 = passStringToWasm0(winner_name, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
-    var len4 = WASM_VECTOR_LEN;
-    wasm.insert_game(ptr0, len0, ptr1, len1, ptr2, len2, ptr3, len3, ptr4, len4);
-}
-
-/**
-* @param {string} player_name
-* @returns {number}
-*/
-export function get_number_of_games(player_name) {
-    var ptr0 = passStringToWasm0(player_name, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
-    var len0 = WASM_VECTOR_LEN;
-    var ret = wasm.get_number_of_games(ptr0, len0);
-    return ret >>> 0;
-}
-
-/**
-* @param {string} player_name
-* @returns {number}
-*/
-export function get_number_of_games_won(player_name) {
-    var ptr0 = passStringToWasm0(player_name, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
-    var len0 = WASM_VECTOR_LEN;
-    var ret = wasm.get_number_of_games_won(ptr0, len0);
-    return ret >>> 0;
-}
-
-/**
-* @param {string} player_name
-* @returns {Array<any>}
-*/
-export function get_all_games(player_name) {
-    var ptr0 = passStringToWasm0(player_name, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
-    var len0 = WASM_VECTOR_LEN;
-    var ret = wasm.get_all_games(ptr0, len0);
-    return takeObject(ret);
-}
-
-/**
-* @param {string} player_name
-* @returns {Array<any>}
-*/
-export function get_all_games_won(player_name) {
-    var ptr0 = passStringToWasm0(player_name, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
-    var len0 = WASM_VECTOR_LEN;
-    var ret = wasm.get_all_games_won(ptr0, len0);
-    return takeObject(ret);
-}
-
-/**
-*/
-export function clear_db() {
-    wasm.clear_db();
-}
-
-/**
-*/
-export function add_data() {
-    wasm.add_data();
-}
-
 export const __wbg_alert_d21aff9a7304d187 = function(arg0, arg1) {
     alert(getStringFromWasm0(arg0, arg1));
 };
@@ -256,25 +142,6 @@ export const __wbindgen_json_serialize = function(arg0, arg1) {
     var len0 = WASM_VECTOR_LEN;
     getInt32Memory0()[arg0 / 4 + 1] = len0;
     getInt32Memory0()[arg0 / 4 + 0] = ptr0;
-};
-
-export const __wbindgen_string_new = function(arg0, arg1) {
-    var ret = getStringFromWasm0(arg0, arg1);
-    return addHeapObject(ret);
-};
-
-export const __wbindgen_object_drop_ref = function(arg0) {
-    takeObject(arg0);
-};
-
-export const __wbg_new_36923be9c90e9812 = function() {
-    var ret = new Array();
-    return addHeapObject(ret);
-};
-
-export const __wbg_push_f5e5628c4c56a8b9 = function(arg0, arg1) {
-    var ret = getObject(arg0).push(getObject(arg1));
-    return ret;
 };
 
 export const __wbindgen_throw = function(arg0, arg1) {
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 #![allow(non_snake_case)]
+#![allow(unused_imports)]
 mod utils;
 
 use wasm_bindgen::prelude::*;
@@ -51,18 +52,15 @@ pub fn check_state(js_ob: &JsValue)->Vec<isize>{
     //alert("Check_State");
     let mut winVal = 0;
     let mut chainVal = 0;
-    let mut temp_r = 0;
-    let mut temp_b = 0;
-    let mut temp_br = 0;
-    let mut temp_tr = 0;
+    
     let state: Vec<Vec<isize>> = js_ob.into_serde().unwrap();
     
     for i in 0 .. 6 {
         for j in 0 .. 7 {
-            temp_r = 0;
-            temp_b = 0;
-            temp_br = 0;
-            temp_tr = 0;
+            let mut temp_r = 0;
+    let mut temp_b = 0;
+    let mut temp_br = 0;
+    let mut temp_tr = 0;
             for k in 0 .. 4 {
                 //from (i,j) to right
                 if j + k < 7 {
@@ -100,147 +98,3 @@ pub fn check_state(js_ob: &JsValue)->Vec<isize>{
     return vec![winVal, chainVal];
 }
 
-#[wasm_bindgen]
-pub fn return_name() -> String{
-    return String::from("testing");
-}
-
-// Want to take in game info and append it to a text file, in the form:
-// game_number~game_type~player1_name~player2_name~winner_name~hour~minute~month~day~year\n
-#[wasm_bindgen]
-pub fn insert_game(game_number: &str, game_type: &str, player1_name: &str, player2_name: &str, winner_name: &str){
-    let local: DateTime<Local> = Local::now();
-
-    let output : String = game_number.to_owned() + &"~".to_owned() + game_type +
-                &"~".to_owned() + player1_name + &"~".to_owned() +  player2_name +
-                &"~".to_owned() + winner_name + &"~".to_owned() + &local.hour().to_string() +
-                &"~".to_owned() + &local.minute().to_string() + &"~".to_owned() + &local.month().to_string() +
-                &"~".to_owned() + &local.day().to_string() + &"~".to_owned() + &local.year().to_string() +
-                &"\n".to_owned();
-
-    let mut file = OpenOptions::new()
-        .write(true)
-        .append(true)
-        .create(true)
-        .open("database.txt")
-        .unwrap();
-
-    file.write(&output.as_bytes()).expect("Unable to write file");
-}
-
-// Want to get the number of games a player has played
-#[wasm_bindgen]
-pub fn get_number_of_games(player_name : &str) -> usize {
-    let data = fs::read("database.txt").expect("Unable to read file");
-    let lines = from_utf8(&data).unwrap().lines();
-    let mut number_of_games : usize = 0;
-    for line in lines{
-        let v: Vec<&str> = line.split('~').collect();
-        if v[2] == player_name || v[3] == player_name {
-            number_of_games = number_of_games + 1;
-        }
-    }
-    number_of_games
-}
-
-// Want to get the number of games a player has won
-#[wasm_bindgen]
-pub fn get_number_of_games_won(player_name : &str) -> usize {
-    let data = fs::read("database.txt").expect("Unable to read file");
-    let lines = from_utf8(&data).unwrap().lines();
-    let mut won : usize = 0;
-    for line in lines{
-        let v: Vec<&str> = line.split('~').collect();
-        if v[4] == player_name {
-            won = won + 1;
-        }
-    }
-    won
-}
-
-// Want to get a game from a line, in the text file format of:
-// game_number~game_type~player1_name~player2_name~winner_name~hour~minute~month~day~year\n
-pub fn get_game_struct(v: Vec<&str>) -> Game {
-    let date = Date {
-        hour: v[5].parse::<u32>().unwrap(),
-        minute: v[6].parse::<u32>().unwrap(),
-        month: v[7].parse::<u32>().unwrap(),
-        day: v[8].parse::<u32>().unwrap(),
-        year: v[9].parse::<i32>().unwrap(),
-    };
-    let game = Game {
-        game_number: v[0].to_string(),
-        game_type: v[1].to_string(),
-        player1_name: v[2].to_string(),
-        player2_name: v[3].to_string(),
-        winner_name: v[4].to_string(),
-        date: date,
-    };
-    game
-}
-
-// Want to get all the games associated with a player and return a vector of strings in JSON format
-#[wasm_bindgen]
-pub fn get_all_games(player_name : &str) -> Array {
-    let mut games : Vec<String> = Vec::new();
-
-    let data = fs::read("database.txt").expect("Unable to read file");
-    let lines = from_utf8(&data).unwrap().lines();
-
-    for line in lines{
-        let v: Vec<&str> = line.split('~').collect();
-        if v[2] == player_name {
-            let v_clone = v.clone();
-            let game : Game = get_game_struct(v_clone);
-            let game_string = serde_json::to_string(&game).unwrap();
-            games.push(game_string);
-        }
-        else if v[3] == player_name {
-            let v_clone = v.clone();
-            let game : Game = get_game_struct(v_clone);
-            let game_string = serde_json::to_string(&game).unwrap();
-            games.push(game_string);
-        }
-    }
-    games.into_iter().map(JsValue::from).collect()
-}
-
-// Want to get all the games won by a player and return a vector of strings in JSON format
-#[wasm_bindgen]
-pub fn get_all_games_won(player_name : &str) -> Array {
-    let mut games : Vec<String> = Vec::new();
-
-    let data = fs::read("database.txt").expect("Unable to read file");
-    let lines = from_utf8(&data).unwrap().lines();
-
-    for line in lines{
-        let v: Vec<&str> = line.split('~').collect();
-        if v[4] == player_name {
-            let v_clone = v.clone();
-            let game : Game = get_game_struct(v_clone);
-            let game_string = serde_json::to_string(&game).unwrap();
-            games.push(game_string);
-        }
-    }
-    games.into_iter().map(JsValue::from).collect()
-}
-
-#[wasm_bindgen]
-pub fn clear_db(){
-    fs::write("database.txt", "").expect("Unable to write file");
-}
-
-// Populate the database with random data
-#[wasm_bindgen]
-pub fn add_data(){
-    insert_game("1", "connect4", "Glenn", "Computer", "Computer");
-    insert_game("2", "connect4", "Andrew", "Computer", "Computer");
-    insert_game("3", "connect4", "Glenn", "Andrew", "Andrew");
-    insert_game("4", "connect4", "Glenn", "Andrew", "Glenn");
-    insert_game("5", "toot", "Micheal", "Computer", "Computer");
-    insert_game("6", "connect4", "Tymoore", "Computer", "Computer");
-    insert_game("7", "connect4", "Micheal", "Tymoore", "Tymoore");
-    insert_game("8", "connect4", "Micheal", "Tymoore", "Micheal");
-    insert_game("9", "toot", "Tymoore", "Andrew", "Tymoore");
-    insert_game("10", "toot", "Glenn", "Micheal", "Glenn");
-}
