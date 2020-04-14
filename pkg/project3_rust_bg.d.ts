e939e5d8635f6dee98780d68f455f0fcb360fa5a
Tymoore Jamal <tymoore@ualberta.ca>
2020-04-14 16:05:54 -0600

    removed scores and scoreboard

--- pkg/project3_rust_bg.d.ts
+++ pkg/project3_rust_bg.d.ts
@@ -3,14 +3,3 @@
 export const memory: WebAssembly.Memory;
 export function greet(): void;
 export function player_move(a: number): number;
-export function return_name(a: number): void;
-export function insert_game(a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number, i: number, j: number): void;
-export function get_number_of_games(a: number, b: number): number;
-export function get_number_of_games_won(a: number, b: number): number;
-export function get_all_games(a: number, b: number): number;
-export function get_all_games_won(a: number, b: number): number;
-export function clear_db(): void;
-export function add_data(): void;
-export function __wbindgen_free(a: number, b: number): void;
-export function __wbindgen_malloc(a: number): number;
-export function __wbindgen_realloc(a: number, b: number, c: number): number;
--- www/scores.js
+++
@@ -1,3 +0,0 @@
-import * as wasm from "Project3Rust";
-
-alert("here in the score board");
